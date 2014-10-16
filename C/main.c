c50c134fe0f43ab71b767a21b8275e42051a3f86
Geoffrey Card <gercunderscore4@gmail.com>
2014-10-16 03:22:14 -0400

    Create main.c

---
+++ C/main.c
@@ -0,0 +1,16 @@
+/* PROGRAM: main.c
+ * PURPOSE: Module for 3D convex hull algorithm
+ * AUTHOR:  Geoffrey Card
+ * DATE:    2014-10-16
+ * NOTES:   Time: O(n^2*log(n))    Algorithm requires sort(n) for each n, hence n*n*log(n).
+ *          Space: O(n)
+ *          Untested.
+ * TO DO:   
+ */
+
+#include <stdio.h>
+#include "ch3d.h"
+
+int main(void) {
+	return 0;
+}
