1cf96fc5c586471d30be128bf65884037d9f59f8
Geoffrey Card <gercunderscore4@gmail.com>
2014-10-16 15:49:35 -0400

    Update ch3d.h

--- C/ch3d.h
+++ C/ch3d.h
@@ -1,11 +1,12 @@
 /* PROGRAM: ch3d.h
  * PURPOSE: Module for 3D convex hull algorithm
  * AUTHOR:  Geoffrey Card
- * DATE:    2014-10-16
+ * DATE:    2014-10-16 - 
  * NOTES:   Time: O(n^2*log(n))    Algorithm requires sort(n) for each n, hence n*n*log(n).
  *          Space: O(n)
  *          Untested.
- * TO DO:   
+ * TO DO:   Write 2D convex hull for planar case.
+ *          Test.
  */
 
 #ifndef CH3D_H_
@@ -42,7 +43,9 @@
  * 
  * PARAMETERS:
  *     float*           a        3D vector
+ * 
  *     float*           b        3D vector
+ * 
  *     float*           c        3D vector, receives result
  *
  * RETURNS:
@@ -60,6 +63,7 @@ void CrossProduct(float* a, float* b, float* c);
  * 
  * PARAMETERS:
  *     float*           a        3D vector
+ * 
  *     float*           b        3D vector
  *
  * RETURNS:
@@ -93,7 +97,9 @@ void UnitVector(float* u);
  * 
  * PARAMETERS:
  *     float*           a        3D vector
+ * 
  *     float*           b        3D vector
+ * 
  *     float*           c        3D vector
  *
  * RETURNS:
@@ -111,11 +117,11 @@ float TripleProduct(float* a, float* b, float* c);
  * 
  * IMPORTANT:
  *     perform srand before this
- *     p MUST BE PRE-ALLOCATED with size 3*size
- *     p IS NOT FREED IN THIS FUNCTION
+ *     assumes p has been PRE-ALLOCATED
  * 
  * PARAMETERS:
  *     float*           p        array for storing points
+ * 
  *     unsigned int     size     number of 3D points (actual size of p is 3*size)
  *
  * RETURNS:
@@ -128,12 +134,9 @@ void GeneratePoints(float* p, unsigned int size, int convexset);
  * DESCRIPTION:
  *     Prints an indexed list of all points.
  * 
- * IMPORTANT:
- *     p MUST BE ALLOCATED with size 3*lenp
- *     p IS NOT FREED IN THIS FUNCTION
- * 
  * PARAMETERS:
  *     float*           p        array of 3D points
+ * 
  *     unsigned int     lenp     number of 3D points (actual size of p is 3*lenp)
  *
  * RETURNS:
@@ -147,16 +150,15 @@ void PrintPoints(float* p, unsigned int lenp);
  *     Prints an indexed list of all points.
  *     Prints a list of all vertex/edge/facet indices and points.
  * 
- * IMPORTANT:
- *     p MUST BE ALLOCATED with size 3*lenp
- *     l MUST BE ALLOCATED with size lenl*leni
- *     p, l ARE NOT FREED IN THIS FUNCTION
- * 
  * PARAMETERS:
  *     float*           p        array of 3D points
+ * 
  *     unsigned int     lenp     number of 3D points (actual size of p is 3*lenp)
+ * 
  *     float*           l        array of point indices
+ * 
  *     unsigned int     lenl     number of listed objects (actual size of p is 3*lenl*leni)
+ * 
  *     unsigned int     leni     number of points per list item (vertex/edge/facet)
  *                               this determines whether the list if of vertices, edges or facets
  *
@@ -165,24 +167,25 @@ void PrintPoints(float* p, unsigned int lenp);
  */
 void PrintResults(float* p, unsigned int lenp, unsigned int* l, unsigned int lenl, unsigned int leni);
 
-/* unsigned int RemoveRepetitions (float* p, unsigned int size)
+/* void RemoveRepetitions (float** p_p, unsigned int* lenp_p)
  * 
  * DESCRIPTION:
  *     Removes repeated points (which would cause problems for the 3D convex hull algorithm).
  *     Returns new number of points.
  * 
  * IMPORTANT:
- *     p MUST BE PRE-ALLOCATED with size 3*size
- *     p IS NOT FREED IN THIS FUNCTION
+ *     I'm quite sure that this has a MEMORY LEAK
+ *         but gcc give me errors when I free the memory
  * 
  * PARAMETERS:
- *     float*           p        array for storing points
- *     unsigned int     size     number of 3D points (actual size of p is size*3)
+ *     float*           p_p      pointer to array for storing points
+ * 
+ *     unsigned int     lenp_p   pointer to number of 3D points (actual size of p is lenp*3)
  *
  * RETURNS:
- *     unsigned int              new value for size
+ *     void
  */
-unsigned int RemoveRepetitions (float* p, unsigned int size);
+void RemoveRepetitions (float** p_p, unsigned int* lenp_p);
 
 /* void SortForIndices(float* l1, unsigned int* s1, unsigned int* s2, unsigned int size)
  * 
@@ -190,14 +193,15 @@ unsigned int RemoveRepetitions (float* p, unsigned int size);
  *     Sorts s1 according to l1 (ascending). l1 remains unsorted.
  * 
  * IMPORTANT:
- *     l1, s1, s2 MUST BE PRE-ALLOCATED with size size
- *     l1, s1, s2 ARE NOT FREED IN THIS FUNCTION
- *     s1 MUST CONTAIN [0, 1, ..., size-1]
+ *     s1 MUST CONTAIN {0, 1, ..., size-1}
  * 
  * PARAMETERS:
  *     float*           l1       unsorted array (there used to be an l2, so I kept the name)
- *     unsigned int*    s1       indices, [0, 1, ..., size-1]
+ * 
+ *     unsigned int*    s1       indices, {0, 1, ..., size-1}
+ * 
  *     unsigned int*    s2       empty list (garbage OK)
+ * 
  *     unsigned int     size     size of l1, s1, s2
  *
  * RETURNS:
@@ -214,12 +218,13 @@ void SortForIndices (float* l1, unsigned int* s1, unsigned int* s2, unsigned int
  *     Checks whether v1 is above, in, or below the plane of v0 and v2: v1 . (v0 x v2) > 0
  *     If it's in the plane, it checks whether v1 is farther away from v than the line between v0 and v2: 
  * 
- * IMPORTANT:
- * 
  * PARAMETERS:
  *     float*           v        3D vector, vii - c (if you don't understand that, read the convex hull algorithm)
+ * 
  *     float*           v0       3D vector
+ * 
  *     float*           v1       3D vector
+ * 
  *     float*           v2       3D vector
  *
  * RETURNS:
@@ -230,7 +235,7 @@ void SortForIndices (float* l1, unsigned int* s1, unsigned int* s2, unsigned int
  */
 unsigned int ConvexityTest (float* v, float* v0, float* v1, float* v2);
 
-/* void ConvexHull3D(float* p, unsigned int* lenp_p, float* l, unsigned int* lenl_p, unsigned int* leni_p, int list_type, int debug)
+/* void ConvexHull3D(float** p_p, unsigned int* lenp_p, unsigned int** l_p, unsigned int* lenl_p, unsigned int* leni_p, int list_type, int debug)
  * 
  * DESCRIPTION:
  *     Removes repeated points from p.
@@ -241,29 +246,27 @@ unsigned int ConvexityTest (float* v, float* v0, float* v1, float* v2);
  *     If debug is TRUE, prints messages to console to help with debugging.
  * 
  * IMPORTANT:
- *     p MUST BE PRE-ALLOCATED with size 3*lenp
- *     l MUST BE UNALLOCATED, it is allocated in this function
- *     p, l are NOT FREED IN THIS FUNCTION
+ *     Planar sets are not yet implemented.
+ *     Not tested beyond triganles.
  * 
  * PARAMETERS:
- *     float*           p        array of 3D points, size 3*lenp
+ *     float**          p_p      pointer to array of 3D points, size 3*lenp
+ * 
  *     unsigned int*    lenp_p   pointer to number of 3D points (actual size of p is 3*lenp)
  *                               it's a pointer so that it can change the original value
- *     unsigned int*    l        array of 3D points, size leni*lenl
+ * 
+ *     unsigned int**   l_p      point to array that receives array of indices, size lenl*leni
+ * 
  *     unsigned int*    lenl_p   pointer to the length of the list (actual size of l is leni*len_p)
  *                               it's a pointer so that it can change the original value
+ * 
  *     unsigned int*    leni_p   pointer to the length of the list type
  *                               1 if it's points, 2 if it's edges, 3 if it's facets
  *                               it's a pointer so that it can change the original value
  *
  * RETURNS:
  *     void
- * 
- * NOTES:
- *     While not yet implemented, I'm going to write in a section that calculates the normals of each test, and marks points that are in non-triangular facets.
- *     If a new facet is found to have marked points, it's normal is compared to a list of non-triangular facet normals.
- *     The list of points adds n memory, the list of facet normals adds 3*(2*V-4). Still linear.
  */
-void ConvexHull3D(float* p, unsigned int* lenp_p, float* l, unsigned int* lenl_p, unsigned int* leni_p, int list_type, int debug);
+void ConvexHull3D(float** p_p, unsigned int* lenp_p, unsigned int** l_p, unsigned int* lenl_p, unsigned int* leni_p, int list_type, int debug);
 
 #endif /* CH3D_H_ */
