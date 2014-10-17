484a6da4f3811f12fae93a07485f7d2d383e35fb
Geoffrey Card <gercunderscore4@gmail.com>
2014-10-17 03:37:45 -0400

    Update main.c

--- C/main.c
+++ C/main.c
@@ -4,7 +4,6 @@
  * DATE:    2014-10-16 - 
  * NOTES:   Time: O(n^2*log(n))    Algorithm requires sort(n) for each n, hence n*n*log(n).
  *          Space: O(n)
- *          Untested.
  * TO DO:   
  */
 
@@ -25,29 +24,37 @@ int main(void) {
 	
 	// seed random with time
 	srand((int) time(NULL));
-	
-	lenp = 3;
+
+	lenp = 10;
 	p = (float*) malloc(3*lenp*sizeof(float));
 	
-	// auto
-	//GeneratePoints(p, lenp, FALSE);
-	// manual
+	GeneratePoints(p, lenp, FALSE);
 	/*
 	for (i = 0; i < lenp; i++) {
-		p[i*3+0] = 0;
-		p[i*3+1] = 0;
-		p[i*3+2] = 0;
+		//p[i*3+0] = 0;
+		p[i*3+1] = 0.5*p[i*3+0];
+		//p[i*3+2] = 0;
 	}
-	*/
-	p[0*3+0] =  1;
+	
+	p[0*3+0] = -1;
 	p[0*3+1] =  0;
 	p[0*3+2] =  0;
-	p[1*3+0] =  0;
-	p[1*3+1] =  1;
+	p[1*3+0] =  1;
+	p[1*3+1] =  0;
 	p[1*3+2] =  0;
 	p[2*3+0] =  0;
-	p[2*3+1] =  0;
-	p[2*3+2] =  1;
+	p[2*3+1] = -1;
+	p[2*3+2] =  0;
+	p[3*3+0] =  0;
+	p[3*3+1] =  1;
+	p[3*3+2] =  0;
+	p[4*3+0] =  0;
+	p[4*3+1] =  0;
+	p[4*3+2] = -1;
+	p[5*3+0] =  0;
+	p[5*3+1] =  0;
+	p[5*3+2] =  1;
+	*/
 	
 	printf("%p %u", p, lenp);
 	printf("\n\n\n");
@@ -55,26 +62,27 @@ int main(void) {
 	PrintPoints(p, lenp);
 	printf("\n\n\n");
 
-	ConvexHull3D(&p, &lenp, &l, &lenl, &leni, VERTICES, TRUE);
+	ConvexHull3D(&p, &lenp, &l, &lenl, &leni, VERTICES);
+	PrintResults(p, lenp, l, lenl, leni);
+	printf("\n\n\n");
+	free(l);
+	l = NULL;
+	
+	ConvexHull3D(&p, &lenp, &l, &lenl, &leni, EDGES);
 	PrintResults(p, lenp, l, lenl, leni);
 	printf("\n\n\n");
 	free(l);
 	l = NULL;
 
-	ConvexHull3D(&p, &lenp, &l, &lenl, &leni, EDGES, TRUE);
+	ConvexHull3D(&p, &lenp, &l, &lenl, &leni, FACETS);
 	PrintResults(p, lenp, l, lenl, leni);
 	printf("\n\n\n");
 	free(l);
 	l = NULL;
-
-	ConvexHull3D(&p, &lenp, &l, &lenl, &leni, FACETS, TRUE);
-	PrintResults(p, lenp, l, lenl, leni);
-	printf("\n\n\n");
-	free(l);
-	l = NULL;
+	
 	free(p);
 	p = NULL;
 	lenp = 0;
-			
+	
 	return 0;
 }
